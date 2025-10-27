d3ff2d8937870cc1857b5e33c909aa5111a22cd5
sirasaki-konoha <rock-db@outlook.jp>
2025-10-27 21:42:03 +0900

    add help and version

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,8 @@ const eql = std.mem.eql;
 const make_hb = @import("make");
 const build = @import("build");
 
+const VERSION = "1.0.0";
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
     const allocator = gpa.allocator();
@@ -23,6 +25,10 @@ pub fn main() !void {
             std.process.exit(1);
         }
         try make_hb.make(allocator, args[2]);
+    } else if (eql(u8, args[1], "help")) {
+        display_help();
+    } else if (eql(u8, args[1], "version")) {
+        std.debug.print("hburg version {s}\n", .{VERSION});
     } else {
         std.debug.print("Unknown command: {s}\n", .{args[1]});
         display_help();
